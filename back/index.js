9498158365c6dad051f58c5be6322715f3c5c940
PoglianiMattia <mattia.pogliani@gmail.com>
2022-02-24 10:49:14 +0100

    add login check from file

--- back/index.js
+++ back/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var apiServer = express();
 var cors = require("cors");
 apiServer.use(cors());
+var fs = require("fs");
 
 var host = "localhost";
 var port = 3000;
@@ -12,9 +13,21 @@ apiServer.listen(port, host, () => {
 
 apiServer.get("/api/login", (req, res) => {
   console.log("ricevuti:", req.query.mail, req.query.password);
-  if(req.query.mail==="mattia" && req.query.password==="pogliani"){
-    res.status(200).json({"message":"login effettuato"});
-  }else{
-    res.status(400).json({"message":"login fallito"});
-  }
+  fs.readFile("users.json", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "errore generico" });
+    } else {
+      var users = JSON.parse(data);
+      users.forEach((a) => {
+        if (
+          a.mail === req.query.mail &&
+          a.password === req.query.password
+        ) {
+          res.status(200).json({ message: "login effettuato" });
+        } else res.status(400).json({ message: "login fallito" });
+      });
+    }
+  });
+  //res.status(500).json({ message: "errore generico" });
 });

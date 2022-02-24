c2a5c0a6b5230634802a23bd468f6cb1d5907264
PoglianiMattia <mattia.pogliani@gmail.com>
2022-02-24 11:08:19 +0100

    add sign up and refactor sign in

--- back/index.js
+++ back/index.js
@@ -3,6 +3,7 @@ var apiServer = express();
 var cors = require("cors");
 apiServer.use(cors());
 var fs = require("fs");
+const { stringify } = require("querystring");
 
 var host = "localhost";
 var port = 3000;
@@ -18,16 +19,33 @@ apiServer.get("/api/login", (req, res) => {
       console.log(err);
       res.status(500).json({ message: "errore generico" });
     } else {
+      var login = false;
       var users = JSON.parse(data);
       users.forEach((a) => {
-        if (
-          a.mail === req.query.mail &&
-          a.password === req.query.password
-        ) {
+        if (a.mail === req.query.mail && a.password === req.query.password) {
           res.status(200).json({ message: "login effettuato" });
-        } else res.status(400).json({ message: "login fallito" });
+          login = true;
+          return;
+        }
+      });
+      if (!login) res.status(400).json({ message: "login failed" });
+    }
+  });
+});
+
+apiServer.get("/api/register", (req, res) => {
+  console.log("ricevuti:", req.query.mail, req.query.password);
+  fs.readFile("users.json", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ message: "errore generico" });
+    } else {
+      var users = JSON.parse(data);
+      users.push({ mail: req.query.mail, password: req.query.password });
+      fs.writeFile("users.json", JSON.stringify(users), (err) => {
+        if (err) res.status(400).json({ message: "sign-up failed" });
+        else res.status(200).json({ message: "sign-up success" });
       });
     }
   });
-  //res.status(500).json({ message: "errore generico" });
 });

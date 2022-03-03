400291f5cfb9a71b5d9818a66efc9442f6424b4e
PoglianiMattia <mattia.pogliani@gmail.com>
2022-03-03 11:35:04 +0100

    add login via db

--- back/index.js
+++ back/index.js
@@ -4,8 +4,15 @@ var cors = require("cors");
 apiServer.use(cors());
 var fs = require("fs");
 const { stringify } = require("querystring");
-require('dotenv').config()
-console.log(process.env.PASSWORD);
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+});
 
 var host = "localhost";
 var port = 3000;
@@ -16,23 +23,22 @@ apiServer.listen(port, host, () => {
 
 apiServer.get("/api/login", (req, res) => {
   console.log("ricevuti:", req.query.mail, req.query.password);
-  fs.readFile("users.json", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ message: "errore generico" });
-    } else {
-      var login = false;
-      var users = JSON.parse(data);
-      users.forEach((a) => {
-        if (a.mail === req.query.mail && a.password === req.query.password) {
-          res.status(200).json({ message: "login effettuato" });
-          login = true;
-          return;
-        }
-      });
-      if (!login) res.status(400).json({ message: "login failed" });
+
+  connection.query(
+    'SELECT count(*) AS utenti FROM c153_5BI1.Users WHERE mail = "' +
+      req.query.mail +
+      '" AND password="' +
+      req.query.password +
+      '";',
+    function (err, results) {
+      console.log(results);
+      if (results[0].utenti >= 1) {
+        res.status(200).json({ message: "login effettuato" });
+      } else {
+        res.status(400).json({ message: "login failed" });
+      }
     }
-  });
+  );
 });
 
 apiServer.get("/api/register", (req, res) => {
